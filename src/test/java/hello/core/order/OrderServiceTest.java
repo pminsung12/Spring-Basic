eb198907fa3e80961506bd9a550785fe8db4b7fc
Minseong Park <pminsung12@gmail.com>
2023-12-30 04:10:56 +0900

    test: 주문 실행 테스트 함수 구현

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        // given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        // when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // then
+        Assertions.assertEquals(order.getDiscountPrice(), 1000);
+    }
+}
