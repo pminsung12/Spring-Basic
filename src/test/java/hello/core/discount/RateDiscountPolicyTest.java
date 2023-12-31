9cb4532ffbb5b6690331a976cd57bbdd520939b7
Minseong Park <pminsung12@gmail.com>
2024-01-01 03:12:23 +0900

    feat: 비율 할인 정책 테스트 구현

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,38 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP should get 10% discount")
+    void vip_o() {
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertEquals(discount, 1000);
+    }
+
+    @Test
+    @DisplayName("If not VIP, then should not get 10% discount")
+    void vip_x() {
+        // given
+        Member member = new Member(2L, "memberBasic", Grade.BASIC);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertEquals(discount, 0);
+    }
+}
