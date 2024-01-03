f28d10eb02e243eed60fde2e641db8f9ebcc54d0
Minseong Park <pminsung12@gmail.com>
2024-01-03 23:55:26 +0900

    feat: AppConfig를 통해 실행함수에서 인터페이스에만 의존하도록 개선

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -4,11 +4,11 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
-import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
+
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
